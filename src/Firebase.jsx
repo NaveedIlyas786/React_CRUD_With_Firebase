970efe37777a54ee22fa97527a6913e965a65283
NaveedIlyas <naveedilyas321@gmail.com>
2023-07-03 12:49:33 +0500

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -2,9 +2,10 @@
 import firebase from "firebase/compat/app"  //! for Firebase V9 we use  "/compat/" to prevent error
 import "firebase/compat/database"
 // Your web app's Firebase configuration
-const firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyAWjIW9e-Vf2HsAzGrLkAZKJ9tPEJGZEqg",
     authDomain: "react-crud-fc5b3.firebaseapp.com",
+    databaseURL: "https://react-crud-fc5b3-default-rtdb.firebaseio.com",
     projectId: "react-crud-fc5b3",
     storageBucket: "react-crud-fc5b3.appspot.com",
     messagingSenderId: "543697026057",
